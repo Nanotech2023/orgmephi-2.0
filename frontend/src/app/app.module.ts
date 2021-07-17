eb021dc52348fea0f530d748e8f0974bb20a5481
Artem Chernyshov <a-chernyshov@protonmail.com>
2021-07-18 00:07:56 +0300

    add: auth module with boilerplates add: webstorm runConfigurations

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,22 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { AppRoutingModule } from '@/app-routing.module'
-import { AppComponent } from '@/app.component'
-import { LayoutModule } from '@/layout/layout.module'
-import { AboutModule } from '@/about/about.module'
-import { FeedbacksModule } from '@/feedbacks/feedbacks.module'
-import { OlympiadsModule } from '@/olympiads/olympiads.module'
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '@/app-routing.module';
+import { AppComponent } from '@/app.component';
+import { LayoutModule } from '@/layout/layout.module';
+import { OlympiadsModule } from '@/olympiads/olympiads.module';
 
 @NgModule( {
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     imports: [
         BrowserModule,
         LayoutModule,
-        AboutModule,
-        FeedbacksModule,
         OlympiadsModule,
-        AppRoutingModule
+        AppRoutingModule,
     ],
     providers: [],
-    bootstrap: [ AppComponent ]
+    bootstrap: [ AppComponent ],
 } )
-export class AppModule
-{
+export class AppModule {
 }
