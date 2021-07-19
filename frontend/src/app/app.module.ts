dc6472033423767d9c7402829fd452513cd5ea69
Artem Chernyshov <a-chernyshov@protonmail.com>
2021-07-19 17:14:57 +0300

    add: ngrx store, reducer and effects fix: add @popperjs/core dependency for boostrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core'
-import { AppRoutingModule } from '@/app-routing.module'
-import { AppComponent } from '@/app.component'
-import { LayoutModule } from '@/layout/layout.module'
-import { OlympiadsModule } from '@/olympiads/olympiads.module'
+import { StoreModule } from '@ngrx/store'
+import { EffectsModule } from '@ngrx/effects'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { SharedModule } from '@/shared/shared.module'
+import { LayoutModule } from '@/layout/layout.module'
 import { AuthModule } from '@/auth/auth.module'
+import { OlympiadsModule } from '@/olympiads/olympiads.module'
+import { AppRoutingModule } from '@/app-routing.module'
+import { reducers } from '@/reducers'
+import { AppEffects } from '@/reducers/app.effects'
+import { environment } from '@environments/environment'
+import { AppComponent } from '@/app.component'
 
 
 @NgModule( {
@@ -17,10 +23,11 @@ import { AuthModule } from '@/auth/auth.module'
         AuthModule,
         OlympiadsModule,
         AppRoutingModule,
+        StoreModule.forRoot( reducers ),
+        EffectsModule.forRoot( [ AppEffects ] ),
+        environment.production ? [] : StoreDevtoolsModule.instrument()
     ],
     providers: [],
     bootstrap: [ AppComponent ]
 } )
-export class AppModule
-{
-}
+export class AppModule {}
