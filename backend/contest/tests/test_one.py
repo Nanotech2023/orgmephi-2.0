b6f69f3c50f9f241b9d9b472309be31220d2940d
Marat Khisamutdinov <maratbest6@gmail.com>
2021-07-20 15:07:56 +0300

    test: add tests config and first try of test

---
+++ backend/contest/tests/test_one.py
@@ -0,0 +1,19 @@
+import pytest
+
+from contest_data.models_responses import Response
+
+
+def test_one(flask_init):
+    print("I'm here!")
+    db = flask_init
+    r = Response(user_id=1, contest_id=1)
+    db.session.add(r)
+    db.session.commit()
+    re = Response.query.all()
+    print(re[0].user_id)
+    assert len(re) == 1
+    print(db.engine.table_names())
+    re = Response.query.all()
+    for p in re:
+        db.session.delete(p)
+    db.session.commit()
