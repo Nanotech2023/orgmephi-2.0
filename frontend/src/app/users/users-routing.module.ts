d0affc8e7a1c26584d141a2369b498aa09374536
Anita Balandina <anita.balandina@gmail.com>
2021-07-20 16:22:53 +0300

    add: auth.guard.service

--- frontend/src/app/users/users-routing.module.ts
+++ frontend/src/app/users/users-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from '@/users/containers/home/home.component'
 import { ParticipantRegistrationFormComponent } from '@/users/components/participant-registration-form/participant-registration-form.component'
+import { AuthGuardService } from '@/users/services/auth.guard.service'
 
 
 const routes: Routes = [
     {
         path: '',
         children: [
-            { path: 'user', component: HomeComponent },
-            { path: 'user/olymp', component: ParticipantRegistrationFormComponent }
+            {
+                path: 'user', component: HomeComponent, canActivate: [ AuthGuardService ]
+            },
+            {
+                path: 'user/olymp', component: ParticipantRegistrationFormComponent, canActivate: [ AuthGuardService ]
+            }
         ]
     }
 ]
@@ -17,7 +22,8 @@ const routes: Routes = [
 
 @NgModule( {
     imports: [ RouterModule.forChild( routes ) ],
-    exports: [ RouterModule ]
+    exports: [ RouterModule ],
+    providers: [ AuthGuardService ]
 } )
 
 export class UsersRoutingModule {}
