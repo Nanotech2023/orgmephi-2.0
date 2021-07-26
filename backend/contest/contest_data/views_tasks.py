f69e7ebb4a053330c8e0a19c9c912e5e9b2b7aa5
Mikhail <16112000m@gmail.com>
2021-07-26 14:11:03 +0300

    add: olympiad removing and getting

--- backend/contest/contest_data/views_tasks.py
+++ backend/contest/contest_data/views_tasks.py
@@ -39,3 +39,33 @@ def olympiad_create():
         db.session.rollback()
         return err.to_response()
 
+
+@app.route('/olympiad/<id_olympiad>/remove', methods=['POST'])
+@openapi
+def olympiad_remove(id_olympiad):
+    try:
+        # Olympiad.query.filter(Olympiad.olympiad_id == id_olympiad).delete()
+        olympiad = Olympiad.query.filter_by(Olympiad.olympiad_id == id_olympiad).one()
+        db.session.delete(olympiad)
+        db.session.commit()
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>', methods=['GET'])
+@openapi
+def olympiad_get(id_olympiad):
+    try:
+       olympiad = Olympiad.query.filter_by(Olympiad.olympiad_id == id_olympiad).one()
+       return make_response(
+           {
+               "olympiad_id": olympiad.id,
+               "name": olympiad.name,
+               "description": olympiad.description,
+               "rules": olympiad.rules,
+           }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
