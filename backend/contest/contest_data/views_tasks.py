e50a33da59488b21075eb141a2f4866c651894b7
Mikhail <16112000m@gmail.com>
2021-07-26 16:48:44 +0300

    add: views for contest and variant

--- backend/contest/contest_data/views_tasks.py
+++ backend/contest/contest_data/views_tasks.py
@@ -94,7 +94,7 @@ def olympiad_update(id_olympiad):
 
 @app.route('/olympiad/all', methods=['GET'])
 @openapi
-def olympiad_update():
+def olympiads_all():
     try:
         olympiads = Olympiad.query.all()
 
@@ -112,7 +112,7 @@ def olympiad_update():
 
         return make_response(
             {
-                "olympiad_list:": all_olympiads
+                "olympiad_list": all_olympiads
             }, 200)
 
     except RequestError as err:
@@ -200,7 +200,7 @@ def stage_update(id_olympiad, id_stage):
 
 @app.route('/olympiad/<id_olympiad>/stage/all', methods=['GET'])
 @openapi
-def olympiad_update():
+def stages_all(id_olympiad):
     try:
         stages = Stage.query.all()
 
@@ -217,7 +217,258 @@ def olympiad_update():
 
         return make_response(
             {
-                "stages_list::": all_stages
+                "stages_list": all_stages
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+# Contest views
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/create', methods=['POST'])
+@openapi
+def contest_create(id_olympiad, id_stage):
+    try:
+        values = request.openapi.body
+        description = values['description']
+        rules = values['rules']
+        winning_condition = values['winning_condition']
+        laureate_condition = values['laureate_condition']
+        certificate_template = values['certificate_template']
+        visibility = values['visibility']
+        start_date = values['start_date']
+        end_time = values['end_time']
+
+        # TODO Checking
+
+        contest = Contest(
+            description=description,
+            rules=rules,
+            winning_condition=winning_condition,
+            laureate_condition=laureate_condition,
+            certificate_template=certificate_template,
+            visibility=visibility,
+            start_date=start_date,
+            end_time=end_time,
+        )
+        db.session.add(contest)
+
+        db.session.commit()
+        return make_response(
+            {
+                "contest_id": contest.contest_id,
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/remove', methods=['POST'])
+@openapi
+def contest_remove(id_olympiad, id_stage, id_contest):
+    try:
+        contest = Contest.query.filter_by(Contest.contest_id == id_contest).one()
+        db.session.delete(contest)
+        db.session.commit()
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>', methods=['GET'])
+@openapi
+def contest_get(id_olympiad, id_stage, id_contest):
+    try:
+        contest = Contest.query.filter_by(Contest.contest_id == id_contest).one()
+        return make_response(
+            {
+                "contest_id": contest.contest_id,
+                "description": contest.description,
+                "rules": contest.rules,
+                "winning_condition": contest.winning_condition,
+                "laureate_condition": contest.laureate_condition,
+                "certificate_template": contest.certificate_template,
+                "visibility": contest.visibility,
+                "start_date": contest.start_date,
+                "end_time": contest.end_time,
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>', methods=['PATCH'])
+@openapi
+def contest_update(id_olympiad, id_stage, id_contest):
+    try:
+        contest = Contest.query.filter_by(Contest.contest_id == id_contest).one()
+
+        values = request.openapi.body
+        if 'description' in values:
+            contest.description = values['description']
+        if 'rules' in values:
+            contest.rules = values['rules']
+        if 'winning_condition' in values:
+            contest.winning_condition = values['winning_condition']
+        if 'laureate_condition' in values:
+            contest.laureate_condition = values['laureate_condition']
+        if 'certificate_template' in values:
+            contest.certificate_template = values['description']
+        if 'description' in values:
+            contest.description = values['description']
+        if 'start_date' in values:
+            contest.start_date = values['start_date']
+        if 'end_time' in values:
+            contest.end_time = values['end_time']
+
+        db.session.commit()
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/all', methods=['GET'])
+@openapi
+def contests_all(id_olympiad, id_stage):
+    try:
+        contests = Contest.query.all()
+
+        all_contests = []
+
+        for contest in contests:
+            all_contests.append(
+                {
+                    "contest_id": contest.contest_id,
+                    "description": contest.description,
+                    "rules": contest.rules,
+                    "winning_condition": contest.winning_condition,
+                    "laureate_condition": contest.laureate_condition,
+                    "certificate_template": contest.certificate_template,
+                    "visibility": contest.visibility,
+                    "start_date": contest.start_date,
+                    "end_time": contest.end_time,
+                }
+            )
+
+        return make_response(
+            {
+                "contests_list": all_contests
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+# Variant views
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/create', methods=['POST'])
+@openapi
+def variant_create(id_olympiad, id_stage, id_contest):
+    try:
+        values = request.openapi.body
+
+        contest = Contest.query.filter_by(Contest.contest_id == id_contest).one()
+        contest.variants.all()
+
+        variant_description = values['variant_description']
+
+        # TODO Checking
+
+        variant = Variant(
+            contest_id=id_contest,
+            variant_number=len(contest),
+            variant_description=variant_description,
+        )
+        db.session.add(contest)
+
+        db.session.commit()
+        return make_response(
+            {
+                "variant_id": variant.variant_id,
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/<id_variant>/remove', methods=['POST'])
+@openapi
+def variant_remove(id_olympiad, id_stage, id_contest, id_variant):
+    try:
+        variant = Variant.query.filter_by(Variant.variant_id == id_variant).one()
+        db.session.delete(variant)
+        db.session.commit()
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/<id_variant>', methods=['GET'])
+@openapi
+def variant_get(id_olympiad, id_stage, id_contest, id_variant):
+    try:
+        variant = Variant.query.filter_by(Variant.variant_id == id_variant).one()
+        return make_response(
+            {
+                "variant_id": variant.variant_id,
+                "variant_number": variant.variant_number,
+                "variant_description": variant.variant_description,
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/<id_variant>', methods=['PATCH'])
+@openapi
+def variant_update(id_olympiad, id_stage, id_contest, id_variant):
+    try:
+        variant = Variant.query.filter_by(Variant.variant_id == id_variant).one()
+
+        values = request.openapi.body
+
+        if 'variant_number' in values:
+            variant.variant_number = values['variant_number']
+        if 'variant_description' in values:
+            variant.variant_description = values['variant_description']
+
+        db.session.commit()
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/all', methods=['GET'])
+@openapi
+def variant_all(id_olympiad, id_stage, id_contest):
+    try:
+        variants = Variant.query.all()
+
+        all_variants = []
+
+        for variant in variants:
+            all_variants.append(
+                {
+                    "variant_id": variant.variant_id,
+                    "variant_number": variant.variant_number,
+                    "variant_description": variant.variant_description,
+                }
+            )
+
+        return make_response(
+            {
+                "variants_list": all_variants
             }, 200)
 
     except RequestError as err:
