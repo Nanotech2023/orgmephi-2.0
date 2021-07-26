e7aa973f1ef8de13ca9aff8700f8cb5109b130dd
Marat Khisamutdinov <maratbest6@gmail.com>
2021-07-26 15:39:36 +0300

    add: user status and mark by user_id

--- backend/contest/contest_data/views_responses.py
+++ backend/contest/contest_data/views_responses.py
@@ -5,6 +5,16 @@ from flask import request, make_response
 from contest_data.models_responses import *
 from contest_data import app, db, openapi
 
+work_status = {
+    'NotChecked': ResponseStatusEnum.not_checked,
+    'Accepted': ResponseStatusEnum.accepted,
+    'Rejected': ResponseStatusEnum.rejected,
+    'Appeal': ResponseStatusEnum.appeal,
+    'Revision': ResponseStatusEnum.revision
+}
+
+work_status_reverse = {val: key for key, val in work_status.items()}
+
 
 @app.route('/olympiad/<olympiad_id>/stage/<stage_id>/contest/<contest_id>/user/<user_id>/response:', methods=['GET'])
 @openapi
@@ -52,7 +62,13 @@ def get_user_answer_by_id(olympiad_id, stage_id, contest_id, answer_id):
            methods=['GET', 'POST'])
 @openapi
 def user_answer_for_task(olympiad_id, stage_id, contest_id, task_id):
-    pass
+    try:  # TODO Add Checking
+        if request.method == 'GET':
+            pass
+        elif request.method == 'POST':
+            pass
+    except Exception as err:  # TODO Add exception
+        pass
 
 
 @app.route('/olympiad/{olympiad_id}/stage/{stage_id}/contest/{contest_id}/task/{task_id}/user/{user_id}:',
@@ -77,6 +93,19 @@ def user_answer_for_task_by_id(olympiad_id, stage_id, contest_id, task_id, user_
             filetype = values['filetype']
             user_work = Response.query.filter_by(user_id=user_id). \
                 filter_by(contest_id=contest_id).one()
+            if user_work is None:
+                user_work = Response(
+                    user_id=user_id,
+                    contest_id=contest_id
+                )
+                db.session.add(user_work)
+                db.session.commit()
+                response_status = ResponseStatus(
+                    work_id=user_work.work_id,
+                    status=work_status['NotChecked']
+                )
+                db.session.add(response_status)
+                db.session.commit()
             user_answer = user_work.answers.filter_by(task_num=task_id).first()
             if user_answer is None:
                 response_answer = ResponseAnswer(
@@ -91,5 +120,62 @@ def user_answer_for_task_by_id(olympiad_id, stage_id, contest_id, task_id, user_
                 user_answer.answer = answer
                 user_answer.filetype = filetype
                 db.session.commit()
+    except Exception as err:  # TODO Add exception
+        pass
+
+
+@app.route('/olympiad/{olympiad_id}/stage/{stage_id}/contest/{contest_id}/user/self/status',
+           methods=['GET', 'POST'])
+@openapi
+def user_status_and_mark_for_response(olympiad_id, stage_id, contest_id):
+    try:  # TODO Add Checking
+        if request.method == 'GET':
+            pass
+        elif request.method == 'POST':
+            pass
+    except Exception as err:  # TODO Add exception
+        pass
+
+
+@app.route('/olympiad/{olympiad_id}/stage/{stage_id}/contest/{contest_id}/user/{user_id}/status',
+           methods=['GET', 'POST'])
+@openapi
+def user_status_and_mark_for_response_by_id(olympiad_id, stage_id, contest_id, user_id):
+    try:  # TODO Add Checking
+        if request.method == 'GET':
+            user_work = Response.query.filter_by(user_id=user_id). \
+                filter_by(contest_id=contest_id).one()
+            status = user_work.statuses.order_by(ResponseStatus.timestamp.desc()).first()
+            if status.mark is None:
+                return make_response(
+                    {
+                        "status": work_status_reverse[status.status]
+                    }, 200)
+            else:
+                return make_response(
+                    {
+                        "status": work_status_reverse[status.status],
+                        "mark": status.mark
+                    }
+                )
+        elif request.method == 'POST':
+            values = request.openapi.body
+            status = values['status']
+            mark = values.get('mark')
+            user_work = Response.query.filter_by(user_id=user_id). \
+                filter_by(contest_id=contest_id).one()
+            if mark is None:
+                response_status = ResponseStatus(
+                    work_id=user_work.work_id,
+                    status=status
+                )
+            else:
+                response_status= ResponseStatus(
+                    work_id=user_work.work_id,
+                    status=status,
+                    mark=mark
+                )
+            db.session.add(response_status)
+            db.session.commit()
     except Exception as err:  # TODO Add exception
         pass
