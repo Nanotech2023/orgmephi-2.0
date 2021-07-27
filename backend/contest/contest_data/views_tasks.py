87edef0a2c2e3f47db87fbc66818c567da8bf677
Mikhail <16112000m@gmail.com>
2021-07-27 10:47:22 +0300

    add: views for users

--- backend/contest/contest_data/views_tasks.py
+++ backend/contest/contest_data/views_tasks.py
@@ -676,11 +676,9 @@ def task_get(id_olympiad, id_stage, id_contest, id_variant, id_task):
 @openapi
 def task_update(id_olympiad, id_stage, id_contest, id_variant, id_task):
     try:
-        task = Task.query.filter_by(Task.task_id == variant_num).one()
+        task = Task.query.filter_by(Task.task_id == id_task).one()
 
         values = request.openapi.body
-
-        task = Task.query.filter_by(Task.task_id == id_task).one()
 
         if 'num_of_task' in values:
             task.num_of_task = values['num_of_task']
@@ -702,26 +700,13 @@ def task_update(id_olympiad, id_stage, id_contest, id_variant, id_task):
                 rangeTask.recommended_answer = values['end_value']
 
         elif task.type == TaskType.plain_task:
-            multipleTask = MultipleTask.query.filter_by(MultipleTask.task_id == id_task).one()
+            # multipleTask = MultipleTask.query.filter_by(MultipleTask.task_id == id_task).one()
 
-            answers = []
-
-            for answer in multipleTask.all_answers_in_multiple_task.all():
-                answers.append({
-                    'answer': answer.answer,
-                    'is_right_answer': answer.correct
-                })
-
-            return make_response(
-                {
-                    "task_id": task.task_id,
-                    "num_of_task": task.num_of_task,
-                    "image_of_task": task.image_of_task,
-                    "answers": answers
-                }, 200)
-
-
-
+            if 'answers' in values:
+                for answer in answers:
+                    answerForTask = AnswersInMultipleChoiceTask.query.filter_by(AnswersInMultipleChoiceTask.answer_id == answer['answer_id']).one()
+                    answerForTask.answer = answer['answer']
+                    answerForTask.correct = answer['is_right_answer']
 
         db.session.commit()
         return make_response({}, 200)
@@ -731,26 +716,25 @@ def task_update(id_olympiad, id_stage, id_contest, id_variant, id_task):
         return err.to_response()
 
 
-@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/all', methods=['GET'])
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/<id_variant>/task/all', methods=['GET'])
 @openapi
-def task_all(id_olympiad, id_stage, id_contest):
+def task_all(id_olympiad, id_stage, id_contest, id_variant):
     try:
-        variants = Variant.query.all()
+        tasks = Task.query.all()
 
-        all_variants = []
+        all_tasks = []
 
-        for variant in variants:
+        for task in tasks:
             all_variants.append(
                 {
-                    "variant_id": variant.variant_id,
-                    "variant_number": variant.variant_number,
-                    "variant_description": variant.variant_description,
+                    "task_id": task.task_id,
+                    "num_of_task": task.num_of_task,
                 }
             )
 
         return make_response(
             {
-                "variants_list": all_variants
+                "tasks_list": all_tasks
             }, 200)
 
     except RequestError as err:
@@ -759,4 +743,122 @@ def task_all(id_olympiad, id_stage, id_contest):
 
 
 
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/variant/<id_variant>/task/<id_task>/taskimage', methods=['GET'])
+@openapi
+def task_image(id_olympiad, id_stage, id_contest, id_variant, id_task):
+    try:
+        tasks = Task.query.filter_by(Task.task_id == id_task).one()
+
+        return make_response(
+            {
+                "task_id": task.task_id,
+                "image_of_task": task.image_of_task
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
 # User views
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/adduser', methods=['POST'])
+@openapi
+def add_user_to_contest(id_olympiad, id_stage, id_contest):
+    try:
+        values = request.openapi.body
+
+        users_id = values['users_id']
+
+        # TODO Checking
+
+        for user_id in users_id:
+            user = UserInContest(
+                user_id=user_id,
+                contest_id=id_contest,
+                variant_id= None, #TODO
+                user_status = None #TODO
+
+            )
+
+            db.session.add(user)
+
+        db.session.commit()
+        return make_response(
+            {
+                "users_id": users_id,
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/{id_contest}/removeuser:', methods=['POST'])
+@openapi
+def remove_user_from_contest(id_olympiad, id_stage, id_contest):
+    try:
+        values = request.openapi.body
+
+        users_id = values['users_id']
+
+        # TODO Checking
+
+        for user_id in users_id:
+            user = UserInContest.query.filter_by(UserInContest.user_id == user_id).one()
+            db.session.delete(user)
+
+        db.session.commit()
+        return make_response({}, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/{id_contest}/user/all', methods=['GET'])
+@openapi
+def users_all(id_olympiad, id_stage, id_contest):
+    try:
+        users = UserInContest.query.filter_by(UserInContest.contest_id == id_contest).one()
+
+        all_users = []
+
+        for user in users:
+            all_users.append(
+                {
+                    "user_id": user.user_id,
+                    "variant_id": user.variant_id,
+                    "user_status": user.user_status,
+                }
+            )
+
+        return make_response(
+            {
+                "user_list": all_users
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
+
+
+@app.route('/olympiad/<id_olympiad>/stage/<id_stage>/contest/<id_contest>/{id_contest}/user/<id_user>/certificate', methods=['GET'])
+@openapi
+def users_all(id_olympiad, id_stage, id_contest, id_user):
+    try:
+        user = UserInContest.query.filter_by(UserInContest.user_id == id_user).one()
+
+        contest = Contest.query.filter_by(Contest.contest_id == id_contest).one()
+
+        # TODO Function
+        certificate = getCertificateFromTemplate(user, contest.certificate_template)
+
+        return make_response(
+            {
+                "certificate": certificate
+            }, 200)
+
+    except RequestError as err:
+        db.session.rollback()
+        return err.to_response()
