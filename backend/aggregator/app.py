3bf853d5b9421571382c43d37ce83640d520203e
Святослав Дмитриев <treplen8@gmail.com>
2021-07-27 21:49:08 +0300

    add: microservice aggregator

---
+++ backend/aggregator/app.py
@@ -0,0 +1,35 @@
+import requests
+from flask import Flask, send_file, redirect, abort
+from flask_cors import CORS, cross_origin
+
+AUTH_HOST = '127.0.0.1:5001'
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/auth/<path:subpath>', methods=['GET', 'POST', 'PUT', 'PATCH'])
+@cross_origin()
+def auth(subpath):
+    return redirect('http://%s/%s' % (AUTH_HOST, subpath), 308)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    try:
+        return send_file('static/index.html')
+    except FileNotFoundError:
+        abort(404)
+
+
+@app.route('/static/<path:subpath>', methods=['GET'])
+def static_file(subpath):
+    try:
+        return send_file('static/%s' % subpath)
+    except FileNotFoundError:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run()
