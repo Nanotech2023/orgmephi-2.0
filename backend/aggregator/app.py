f6065c58305b52054e89664c0eb427c6dcf11b25
Святослав Дмитриев <treplen8@gmail.com>
2021-07-28 16:05:47 +0300

    add: setting for frontend directory

--- backend/aggregator/app.py
+++ backend/aggregator/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, send_file, redirect, abort
 from flask_cors import CORS, cross_origin
+import os
 
 AUTH_HOST = '127.0.0.1:5001'
+FRONTEND_DIR_REL = 'frontend/dist'
+
+if FRONTEND_DIR_REL[0] == '/':
+    FRONTEND_DIR = FRONTEND_DIR_REL
+else:
+    FRONTEND_DIR = '%s/%s' % (os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))),
+                              FRONTEND_DIR_REL)
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -14,18 +22,18 @@ def auth(subpath):
     return redirect('http://%s/%s' % (AUTH_HOST, subpath), 308)
 
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/web/<path:subpath>', methods=['GET'])
+def static_file(subpath):
     try:
-        return send_file('static/index.html')
+        return send_file('%s/%s' % (FRONTEND_DIR, subpath))
     except FileNotFoundError:
         abort(404)
 
 
-@app.route('/static/<path:subpath>', methods=['GET'])
-def static_file(subpath):
+@app.route('/', methods=['GET'])
+def index():
     try:
-        return send_file('static/%s' % subpath)
+        return send_file('%s/index.html' % FRONTEND_DIR)
     except FileNotFoundError:
         abort(404)
 
