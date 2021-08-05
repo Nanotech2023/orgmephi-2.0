5410a59b2058e6dc3b6c8694f97c381657150a86
Marat Khisamutdinov <maratbest6@gmail.com>
2021-08-05 20:53:36 +0300

    refactor: cycle vectorizing, better model serialisation

--- backend/contest/responses/views.py
+++ backend/contest/responses/views.py
@@ -1,10 +1,10 @@
 from flask import request, make_response
 from .models import *
 from contest.tasks.models import CompositeContest
-from common.errors import RequestError, NotFound, InsufficientData
+from common.errors import NotFound, InsufficientData
 from common import get_current_app, get_current_module
 from common.jwt_verify import jwt_required, jwt_required_role, jwt_get_id
-from common.util import db_get_or_raise
+from common.util import db_get_or_raise, db_get_list
 
 db = get_current_db()
 module = get_current_module()
@@ -19,33 +19,49 @@ def get_user_in_contest_work(user_id, contest_id):
     return user_work
 
 
-def get_missing(values, search):
-    missing = []
-    for value in search:
-        if value not in values:
-            missing.append(value)
-    return missing
-
-
 def check_olympiad_and_stage(olympiad_id, stage_id, contest_id):
     olympiad = db_get_or_raise(CompositeContest, 'contest_id', olympiad_id)
     stages = olympiad.stages
-    found = False
-    our_stage = None
-    for stage in stages:
-        if stage.stage_id == stage_id:
-            our_stage = stage
-            found = True
-    if not found:
+    our_stage = next((stage for stage in stages if stage.stage_id == stage_id), None)
+    if our_stage is None:
         raise NotFound('olympiad.stage', 'for stage %d' % stage_id)
     contests = our_stage.contests
-    found = False
-    for contest in contests:
-        if contest.contest_id == contest_id:
-            found = True
-    if not found:
+    our_contest = next((contest for contest in contests if contest.contest_id == contest_id), None)
+    if our_contest is None:
         raise NotFound('olympiad.stage.contest', 'for contest %d' % contest_id)
-    return ''
+
+
+def user_answer_get(user_id, contest_id, task_id):
+    user_work = get_user_in_contest_work(user_id, contest_id)
+    if user_work.answers is None:
+        raise NotFound('user_response.answers', 'for user %d' % user_id)
+    user_answer = user_work.answers.filter(task_num=task_id).one_or_none()
+    if user_answer is None:
+        raise NotFound('response_answer', 'for task_id %d' % task_id)
+    return {
+            "user_answer": user_answer.answer,
+            "filetype": user_answer.filetype.value
+    }
+
+
+def user_answer_post(values, user_id, contest_id, task_id):
+    search = ['user_answer', 'filetype']
+    missing = [value for value in search if value not in values]
+    if len(missing) > 0:
+        raise InsufficientData(str(missing), 'for user %d' % user_id)
+    answer = values['user_answer']
+    filetype = values['filetype']
+    try:
+        user_work = get_user_in_contest_work(user_id, contest_id)
+    except NotFound:
+        user_work = add_user_response(db.session, user_id, contest_id)
+        response_status = add_response_status(db.session, user_work.work_id)
+    user_answer = user_work.answers.filter(task_num=task_id).one_or_none()
+    if user_answer is None:
+        response_answer = add_response_answer(db.session, user_work.work_id, task_id, answer, filetype)
+    else:
+        user_answer.update(answer_new=answer, filetype_new=filetype)
+    db.session.commit()
 
 
 @module.route('/olympiad/<int:olympiad_id>/stage/<int:stage_id>/contest/<int:contest_id>/user/<int:user_id>/response:',
@@ -57,9 +73,7 @@ def get_user_all_answers(olympiad_id, stage_id, contest_id, user_id):
     if user_work.answers is None:
         raise NotFound('user_response.answers', 'for user %d' % user_id)
     answers = user_work.answers.all()
-    user_answer = []
-    for elem in answers:
-        user_answer.append(elem.serialize())
+    user_answer = [elem.serialize() for elem in answers]
     return make_response(
         {
             "user_id": user_work.user_id,
@@ -89,35 +103,11 @@ def user_answer_for_task(olympiad_id, stage_id, contest_id, task_id):
     check_olympiad_and_stage(olympiad_id, stage_id, contest_id)
     self_user_id = jwt_get_id()
     if request.method == 'GET':
-        user_work = get_user_in_contest_work(self_user_id, contest_id)
-        if user_work.answers is None:
-            raise NotFound('user_response.answers', 'for user %d' % self_user_id)
-        user_answer = user_work.answers.filter(task_num=task_id).one_or_none()
-        if user_answer is None:
-            raise NotFound('response_answer', 'for task_id %d' % task_id)
-        return make_response(
-            {
-                "user_answer": user_answer.answer,
-                "filetype": filetype_reverse[user_answer.filetype]
-            }, 200)
+        user_answer = user_answer_get(self_user_id, contest_id, task_id)
+        return make_response(user_answer, 200)
     elif request.method == 'POST':
         values = request.openapi.body
-        missing = get_missing(values, ['user_answer', 'filetype'])
-        if len(missing) > 0:
-            raise InsufficientData(str(missing), 'for user %d' % self_user_id)
-        answer = values['user_answer']
-        filetype = values['filetype']
-        try:
-            user_work = get_user_in_contest_work(self_user_id, contest_id)
-        except NotFound:
-            user_work = add_user_response(db.session, self_user_id, contest_id)
-            response_status = add_response_status(db.session, user_work.work_id)
-        user_answer = user_work.answers.filter(task_num=task_id).one_or_none()
-        if user_answer is None:
-            response_answer = add_response_answer(db.session, user_work.work_id, task_id, answer, filetype)
-        else:
-            user_answer.update(answer_new=answer, filetype_new=filetype)
-        db.session.commit()
+        user_answer_post(values, self_user_id, contest_id, task_id)
         return make_response({}, 200)
 
 
@@ -128,35 +118,11 @@ def user_answer_for_task(olympiad_id, stage_id, contest_id, task_id):
 def user_answer_for_task_by_id(olympiad_id, stage_id, contest_id, task_id, user_id):
     check_olympiad_and_stage(olympiad_id, stage_id, contest_id)
     if request.method == 'GET':
-        user_work = get_user_in_contest_work(user_id, contest_id)
-        if user_work.answers is None:
-            raise NotFound('user_response.answers', 'for user %d' % user_id)
-        user_answer = user_work.answers.filter(task_num=task_id).one_or_none()
-        if user_answer is None:
-            raise NotFound('response_answer', 'for task_id %d' % task_id)
-        return make_response(
-            {
-                "user_answer": user_answer.answer,
-                "filetype": filetype_reverse[user_answer.filetype]
-            }, 200)
+        user_answer = user_answer_get(user_id, contest_id, task_id)
+        return make_response(user_answer, 200)
     elif request.method == 'POST':
         values = request.openapi.body
-        missing = get_missing(values, ['user_answer', 'filetype'])
-        if len(missing) > 0:
-            raise InsufficientData(str(missing), 'for user %d' % user_id)
-        answer = values['user_answer']
-        filetype = values['filetype']
-        try:
-            user_work = get_user_in_contest_work(user_id, contest_id)
-        except NotFound:
-            user_work = add_user_response(db.session, user_id, contest_id)
-            response_status = add_response_status(db.session, user_work.work_id)
-        user_answer = user_work.answers.filter(task_num=task_id).one_or_none()
-        if user_answer is None:
-            response_answer = add_response_answer(db.session, user_work.work_id, task_id, answer, filetype)
-        else:
-            user_answer.update(answer_new=answer, filetype_new=filetype)
-        db.session.commit()
+        user_answer_post(values, user_id, contest_id, task_id)
         return make_response({}, 200)
 
 
@@ -219,7 +185,7 @@ def user_status_history_for_response(olympiad_id, stage_id, contest_id):
     self_user_id = jwt_get_id()
     user_work = get_user_in_contest_work(self_user_id, contest_id)
     status = user_work.statuses.order_by(ResponseStatus.timestamp.desc())
-    history = get_status_history(db.session, user_work.work_id, status)
+    history = [elem.status_for_history() for elem in status]
     return make_response(
         {
             'user_id': self_user_id,
@@ -236,7 +202,7 @@ def user_status_history_for_response_by_id(olympiad_id, stage_id, contest_id, us
     check_olympiad_and_stage(olympiad_id, stage_id, contest_id)
     user_work = get_user_in_contest_work(user_id, contest_id)
     status = user_work.statuses.order_by(ResponseStatus.timestamp.desc())
-    history = get_status_history(db.session, user_work.work_id, status)
+    history = [elem.status_for_history() for elem in status]
     return make_response(
         {
             'user_id': user_id,
@@ -249,15 +215,8 @@ def user_status_history_for_response_by_id(olympiad_id, stage_id, contest_id, us
 @jwt_required_role(['Admin', 'System', 'Creator'])
 def get_list_for_stage(olympiad_id, stage_id, contest_id):
     check_olympiad_and_stage(olympiad_id, stage_id, contest_id)
-    users_in_contest = get_list(Response, 'contest_id', contest_id)
-    user_rows = []
-    for elem in users_in_contest:
-        mark = ResponseStatus.query.filter(work_id=elem.work_id).order_by(ResponseStatus.timestamp.desc()).one().mark
-        user_info = {
-            'user_id': elem.user_id,
-            'mark': mark
-        }
-        user_rows.append(user_info)
+    users_in_contest = db_get_list(Response, 'contest_id', contest_id)
+    user_rows = [elem.prepare_for_list() for elem in users_in_contest]
     return make_response(
         {
             'contest_id': contest_id,
@@ -331,7 +290,8 @@ def user_response_appeal_info_by_id(olympiad_id, stage_id, contest_id, user_id):
 def reply_to_user_appeal(olympiad_id, stage_id, contest_id, appeal_id):
     check_olympiad_and_stage(olympiad_id, stage_id, contest_id)
     values = request.openapi.body
-    missing = get_missing(values, ['message', 'accepted'])
+    search = ['message', 'accepted']
+    missing = [value for value in search if value not in values]
     if len(missing) > 0:
         raise InsufficientData(str(missing), 'for appeal %d' % appeal_id)
     message = values['message']
