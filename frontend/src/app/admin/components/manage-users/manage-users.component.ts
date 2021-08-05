ab33653a14e1828541d34cb7e6b2838a48bf638c
Anita Balandina <anita.balandina@gmail.com>
2021-08-05 18:02:59 +0300

    add: modal to manage-users.component

--- frontend/src/app/admin/components/manage-users/manage-users.component.ts
+++ frontend/src/app/admin/components/manage-users/manage-users.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { AuthServiceMock } from '@/auth/api/auth.service.mock'
-import { ResponseUserAll, TypeCommonName, TypeIdentifier, TypeUserRole, TypeUserType } from '@/auth/models'
+import { ResponseUserAll } from '@/auth/models'
 import { Observable } from 'rxjs'
-import { fixedHeight, footerHeight, headerHeight } from '@/shared/consts'
+import { fixedHeight } from '@/shared/consts'
 
 
 @Component( {
     selector: 'app-manage-users',
     templateUrl: './manage-users.component.html',
-    styleUrls: [ './manage-users.component.scss' ]
+    styleUrls: [ './manage-users.component.scss' ],
+    host: {
+        '(document:click)': 'onClick($event)'
+    }
 } )
+
+
 export class ManageUsersComponent implements OnInit
 {
     users$!: Observable<ResponseUserAll>
     minContainerHeight = fixedHeight
+    @ViewChild( 'modal' ) modal!: ElementRef
     columnDefs = [
         { field: 'id', sortable: true, filter: true, headerName: 'ID' },
         { field: 'username', sortable: true, filter: true, headerName: 'Имя пользовтаеля' },
@@ -21,11 +27,18 @@ export class ManageUsersComponent implements OnInit
         { field: 'type', sortable: true, filter: true, headerName: 'Тип регистрации' },
         { headerName: 'Действия', cellRenderer: 'btnCellRenderer' }
     ]
+    modalVisible: boolean = false
 
     constructor( private service: AuthServiceMock ) { }
 
     ngOnInit(): void
     {
         this.users$ = this.service.userAllGet()
+    }
+
+    onClick( $event: any )
+    {
+        if ( $event.target == this.modal?.nativeElement )
+            this.modalVisible = false
     }
 }
