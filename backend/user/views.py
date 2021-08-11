d510b011753231037f08e8e1c0e702853366c401
Святослав Дмитриев <treplen8@gmail.com>
2021-08-11 22:24:16 +0300

    refactor: create registration module

--- backend/user/views.py
+++ backend/user/views.py
@@ -16,23 +16,13 @@ db = get_current_db()
 module = get_current_module()
 app = get_current_app()
 
-user_roles = {role.value: role for role in UserRoleEnum}
 
-user_types = {user_type.value: user_type for user_type in UserTypeEnum}
+
+
 
 
 def get_missing(values, search):
     return [value for value in search if value not in values]
-
-
-def grade_to_year(grade):
-    from datetime import date
-    now = datetime.utcnow().date()
-    last_admission = date(now.year, 9, 1)
-    if now < last_admission:
-        last_admission = date(now.year - 1, 9, 1)
-    admission_date = date(last_admission.year - grade + 1, 9, 1)
-    return admission_date
 
 
 def update_password(user_id, new_password, old_password, admin=False):
@@ -61,47 +51,6 @@ def generate_refresh_token(user_id, remember_me):
         refresh_token = create_refresh_token(identity=user_id, additional_claims=additional_claims)
     csrf_refresh_token = get_csrf_token(refresh_token)
     return refresh_token, csrf_refresh_token
-
-
-# Registration
-
-def register():
-    values = request.openapi.body
-    username = values['auth_info']['email']
-    reg_type = user_types[values['register_type']]
-    password_hash = app.password_policy.hash_password(values['auth_info']['password'], check=True)
-
-    user_data = values['personal_info']
-
-    try:
-        user = add_user(db.session, username, password_hash, UserRoleEnum.participant, reg_type)
-        add_personal_info(db.session, user, username, user_data['first_name'], user_data['second_name'],
-                          user_data['middle_name'], user_data['date_of_birth'])
-
-        if reg_type == UserTypeEnum.university:
-            student_data = values['student_info']
-            add_university_info(db.session, user, student_data['phone_number'], student_data['university'],
-                                grade_to_year(student_data['grade']), student_data['university_country'],
-                                student_data['citizenship'], student_data['region'], student_data['city'])
-
-        db.session.commit()
-    except sqlalchemy.exc.IntegrityError:
-        raise AlreadyExists('username', username)
-    except Exception:
-        db.session.rollback()
-        raise
-    return make_response(user.serialize(), 200)
-
-
-@module.route('/register/school', methods=['POST'])
-def register_school():
-    return register()
-
-
-@module.route('/register/university', methods=['POST'])
-def register_university():
-    return register()
-
 
 @module.route('/register/internal', methods=['POST'])
 @jwt_required_role(['Admin', 'System'])
@@ -444,15 +393,4 @@ def remove_user_groups(user_id):
 
 # Reference Information
 
-@module.route('/info/universities', methods=['GET'])
-def get_universities():
-    universities = get_all(University)
-    university_list = [uni.name for uni in universities]
-    return make_response({'university_list': university_list}, 200)
 
-
-@module.route('/info/countries', methods=['GET'])
-def get_countries():
-    countries = get_all(Country)
-    country_list = [country.name for country in countries]
-    return make_response({'country_list': country_list}, 200)
