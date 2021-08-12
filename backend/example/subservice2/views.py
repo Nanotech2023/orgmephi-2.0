8e051880afb57a34beaa7762d8d881446036f963
sodmitriev <treplen8@gmail.com>
2021-08-12 06:01:22 +0300

    add: apispec enum support

--- backend/example/subservice2/views.py
+++ backend/example/subservice2/views.py
@@ -1,5 +1,8 @@
+from enum import Enum
+
 from flask import request
 from marshmallow import Schema, fields
+from marshmallow_enum import EnumField, LoadDumpOptions
 
 from common import get_current_module
 
@@ -10,13 +13,34 @@ class InputSchema(Schema):
     now = fields.DateTime(required=True)
 
 
+class TimeEnum(Enum):
+    morning = 'morning'
+    day = 'day'
+    evening = 'evening'
+    night = 'night'
+
+
 class OutputSchema(Schema):
-    msg = fields.Str(required=True)
+    msg = fields.Str(required=True, dump_only=True)
+    time = EnumField(TimeEnum, required=True, dump_by=LoadDumpOptions.value)
 
 
 class Output:
-    def __init__(self, msg):
-        self.msg = msg
+    def __init__(self, time):
+        self.time = time
+
+    @property
+    def msg(self):
+        if self.time == TimeEnum.morning:
+            return 'Good morning'
+        elif self.time == TimeEnum.day:
+            return 'Good afternoon'
+        elif self.time == TimeEnum.evening:
+            return 'Good evening'
+        elif self.time == TimeEnum.night:
+            return 'ZzZzZ...'
+        else:
+            return '???'
 
 
 @module.route('/hello', methods=['POST'], input_schema=InputSchema, output_schema=OutputSchema)
@@ -38,11 +62,11 @@ def hello():
     """
     time = request.marshmallow['now']
     if 6 <= time.hour < 12:
-        msg = 'Good morning'
+        t = TimeEnum.morning
     elif 12 <= time.hour < 18:
-        msg = 'Good afternoon'
+        t = TimeEnum.day
     elif 18 <= time.hour < 24:
-        msg = 'Good evening'
+        t = TimeEnum.evening
     else:
-        msg = 'ZzZzZ...'
-    return Output(msg), 200
+        t = TimeEnum.night
+    return Output(t), 200
