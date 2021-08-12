551b8998fe8f17a22b9bb8e02bcddc2acf2d8934
Mikhail <16112000m@gmail.com>
2021-08-12 17:36:20 +0300

    fix: participant module

--- backend/contest/tasks/participant/views.py
+++ backend/contest/tasks/participant/views.py
@@ -1,76 +1,12 @@
 from flask import abort, make_response
 
-from common.errors import InsufficientData
 from common import get_current_app, get_current_module
-from common.util import db_get_or_raise, db_get_all
-from common.jwt_verify import jwt_get_id
-
-from contest.tasks.models import *
+from common.util import db_get_all
+from contest.tasks.util import *
 
 db = get_current_db()
 module = get_current_module()
 app = get_current_app()
-
-
-# Functions
-
-
-def is_user_in_contest(user_id, contest):
-    if contest.users.filter_by(**{"user_id": str(user_id)}).one_or_none() is not None:
-        return True
-    else:
-        return False
-
-
-def is_variant_in_contest(variant_id, contest):
-    if contest.variants.filter_by(**{"variant_id": str(variant_id)}).one_or_none() is not None:
-        return True
-    else:
-        return False
-
-
-def is_task_in_variant(task_id, variant):
-    if variant.tasks.filter_by(**{"task_id": str(task_id)}).one_or_none() is not None:
-        return True
-    else:
-        return False
-
-
-def get_user_contest_if_possible(id_olympiad, id_stage, id_contest):
-    olympiad = db_get_or_raise(Contest, "contest_id", id_olympiad)
-    stage = db_get_or_raise(Stage, "stage_id", str(id_stage))
-    if stage not in olympiad.stages:
-        raise InsufficientData('stage_id', 'not in current olympiad')
-    contest = db_get_or_raise(Stage, "contest_id", str(id_contest))
-    if contest not in stage.contests:
-        raise InsufficientData('contest_id', 'not in current stage')
-    if not is_user_in_contest(jwt_get_id(), contest):
-        raise InsufficientData('contest_id', 'not in your contests list')
-    return contest
-
-
-def get_user_variant_if_possible(id_olympiad, id_stage, id_contest):
-    contest = get_user_contest_if_possible(id_olympiad, id_stage, id_contest)
-    user = db_get_or_raise(UserInContest, "user_id", jwt_get_id())
-    variant = contest.variants.filter_by(**{"variant_id": str(user.variant_id)}).one_or_none()
-    if not is_variant_in_contest(variant.variant_id, contest):
-        raise InsufficientData('variant_id', 'not in current contests')
-    return variant
-
-
-def get_user_tasks_if_possible(id_olympiad, id_stage, id_contest):
-    variant = get_user_variant_if_possible(id_olympiad, id_stage, id_contest)
-    tasks = [task.serialize() for task in variant.tasks]
-    return tasks
-
-
-def get_user_task_if_possible(id_olympiad, id_stage, id_contest, id_task):
-    variant = get_user_variant_if_possible(id_olympiad, id_stage, id_contest)
-    if is_task_in_variant(id_task, variant):
-        task = db_get_or_raise(Task, "task_id", str(id_task))
-        return task
-    else:
-        raise InsufficientData('task_id', 'not in current variant')
 
 
 # Olympiad types
@@ -85,6 +21,12 @@ def olympiad_type_all():
     all_olympiad_types = [olympiad_type.serialize() for olympiad_type in olympiad_types]
     return make_response(
         {"olympiad_types": all_olympiad_types}, 200)
+
+
+@module.route('/olympiad_type/<int:id_olympiad_type>', methods=['GET'])
+def olympiad_type_get(id_olympiad_type):
+    olympiad = db_get_or_raise(OlympiadType, "olympiad_type_id", str(id_olympiad_type))
+    return make_response(olympiad.serialize(), 200)
 
 
 # Olympiad
@@ -203,7 +145,7 @@ def contest_self(id_olympiad, id_stage, id_contest):
 
 
 @module.route(
-    '/olympiad/<int:id_olympiad>/stage/<int:id_stage>/contest/<int:id_contest>/variant/self',
+    '/contest/<int:id_contest>/variant/self',
     methods=['GET'])
 def variant_self(id_olympiad, id_stage, id_contest):
     """
@@ -218,7 +160,7 @@ def variant_self(id_olympiad, id_stage, id_contest):
 
 
 @module.route(
-    '/olympiad/<int:id_olympiad>/stage/<int:id_stage>/contest/<int:id_contest>/tasks/self',
+    '/contest/<int:id_contest>/tasks/self',
     methods=['GET'])
 def task_all(id_olympiad, id_stage, id_contest):
     """
@@ -232,7 +174,7 @@ def task_all(id_olympiad, id_stage, id_contest):
 
 
 @module.route(
-    '/olympiad/<int:id_olympiad>/stage/<int:id_stage>/contest/<int:id_contest>/tasks/<int:id_task>',
+    '/contest/<int:id_contest>/tasks/<int:id_task>/self',
     methods=['GET'])
 def task_get(id_olympiad, id_stage, id_contest, id_task):
     """
@@ -244,7 +186,7 @@ def task_get(id_olympiad, id_stage, id_contest, id_task):
 
 
 @module.route(
-    '/olympiad/<int:id_olympiad>/stage/<int:id_stage>/contest/<int:id_contest>/tasks/<int:id_task>/image',
+    '/contest/<int:id_contest>/tasks/<int:id_task>/image/self',
     methods=['GET'])
 def task_image(id_olympiad, id_stage, id_contest, id_task):
     """
@@ -259,7 +201,7 @@ def task_image(id_olympiad, id_stage, id_contest, id_task):
 
 
 @module.route(
-    '/olympiad/<int:id_olympiad>/stage/<int:id_stage>/contest/<int:id_contest>/certificate',
+    '/olympiad/<int:id_olympiad>/stage/<int:id_stage>/contest/<int:id_contest>/certificate/self',
     methods=['GET'])
 def users_certificate(id_olympiad, id_stage, id_contest):
     # contest = get_user_contest_if_possible(id_olympiad, id_stage, id_contest)
---
+++ backend/contest/tasks/util.py
@@ -0,0 +1,146 @@
+from common.errors import InsufficientData
+from common.util import db_get_or_raise
+from common.jwt_verify import jwt_get_id
+
+from contest.tasks.models import *
+
+import random
+
+# Generators
+
+
+def get_last_variant_in_contest(contest):
+    variants = contest.variants.all()
+    if len(variants) > 0:
+        new_variant = max(variant.variant_number for variant in variants)
+    else:
+        new_variant = 0
+
+    return new_variant
+
+
+def generate_variant(id_contest, user_id):
+    contest = db_get_or_raise(Contest, "contest_id", id_contest)
+    variants_number = len(contest.variants.all())
+    if variants_number == 0:
+        raise InsufficientData('variant', 'variants in contest')
+    random_number = random.randint(0, variants_number * 2)
+    variant = (user_id + random_number) % variants_number
+    return variant
+
+# Functions for tasks/participant
+
+
+def is_user_in_contest(user_id, contest):
+    if contest.users.filter_by(**{"user_id": str(user_id)}).one_or_none() is not None:
+        return True
+    else:
+        return False
+
+
+def is_variant_in_contest(variant_id, contest):
+    if contest.variants.filter_by(**{"variant_id": str(variant_id)}).one_or_none() is not None:
+        return True
+    else:
+        return False
+
+
+def is_task_in_variant(task_id, variant):
+    if variant.tasks.filter_by(**{"task_id": str(task_id)}).one_or_none() is not None:
+        return True
+    else:
+        return False
+
+
+def get_user_contest_if_possible(id_olympiad, id_stage, id_contest):
+    olympiad = db_get_or_raise(Contest, "contest_id", id_olympiad)
+    stage = db_get_or_raise(Stage, "stage_id", str(id_stage))
+    if stage not in olympiad.stages:
+        raise InsufficientData('stage_id', 'not in current olympiad')
+    contest = db_get_or_raise(Stage, "contest_id", str(id_contest))
+    if contest not in stage.contests:
+        raise InsufficientData('contest_id', 'not in current stage')
+    if not is_user_in_contest(jwt_get_id(), contest):
+        raise InsufficientData('contest_id', 'not in your contests list')
+    return contest
+
+
+def get_user_variant_if_possible(id_olympiad, id_stage, id_contest):
+    contest = get_user_contest_if_possible(id_olympiad, id_stage, id_contest)
+    user = db_get_or_raise(UserInContest, "user_id", jwt_get_id())
+    variant = contest.variants.filter_by(**{"variant_id": str(user.variant_id)}).one_or_none()
+    if not is_variant_in_contest(variant.variant_id, contest):
+        raise InsufficientData('variant_id', 'not in current contests')
+    return variant
+
+
+def get_user_tasks_if_possible(id_olympiad, id_stage, id_contest):
+    variant = get_user_variant_if_possible(id_olympiad, id_stage, id_contest)
+    tasks = [task.serialize() for task in variant.tasks]
+    return tasks
+
+
+def get_user_task_if_possible(id_olympiad, id_stage, id_contest, id_task):
+    variant = get_user_variant_if_possible(id_olympiad, id_stage, id_contest)
+    if is_task_in_variant(id_task, variant):
+        task = db_get_or_raise(Task, "task_id", str(id_task))
+        return task
+    else:
+        raise InsufficientData('task_id', 'not in current variant')
+
+
+# Functions for tasks/contest
+
+def get_contest_if_possible_from_stage(id_olympiad, id_stage,id_contest):
+    olympiad = db_get_or_raise(Contest, "contest_id", id_olympiad)
+    stage = db_get_or_raise(Stage, "stage_id", str(id_stage))
+    contest = db_get_or_raise(Stage, "contest_id", str(id_contest))
+    if stage not in olympiad.stages:
+        raise InsufficientData('stage_id', 'not in current olympiad')
+    if contest not in stage.contests:
+        raise InsufficientData('contest_id', 'not in current stage')
+    return contest
+
+
+def get_contest_if_possible(id_contest):
+    contest = db_get_or_raise(Stage, "contest_id", str(id_contest))
+    return contest
+
+
+def get_variant_if_possible(id_contest, id_variant):
+    contest = db_get_or_raise(Stage, "contest_id", str(id_contest))
+    variant = contest.variants.filter_by(**{"variant_id": str(id_variant)}).one_or_none()
+    if not is_variant_in_contest(variant.variant_id, contest):
+        raise InsufficientData('variant_id', 'not in current contests')
+    return variant
+
+
+def get_variant_if_possible_by_number(id_contest, variant_num):
+    contest = db_get_or_raise(Stage, "contest_id", str(id_contest))
+    variant = contest.variants.filter_by(**{"variant_number": str(variant_num)}).one_or_none()
+    if not is_variant_in_contest(variant.variant_id, contest):
+        raise InsufficientData('variant_number', 'not in current contests')
+    return variant
+
+
+def get_tasks_if_possible(id_contest, id_variant):
+    variant = get_variant_if_possible(id_contest, id_variant)
+    tasks = [task.serialize() for task in variant.tasks]
+    return tasks
+
+
+def get_task_if_possible(id_contest, id_variant, id_task):
+    variant = get_variant_if_possible(id_contest, id_variant)
+    if is_task_in_variant(id_task, variant):
+        task = db_get_or_raise(Task, "task_id", str(id_task))
+        return task
+    else:
+        raise InsufficientData('task_id', 'not in current variant')
+
+
+# Validators
+
+def validate_contest_values(previous_contest_id, previous_participation_condition):
+    if (previous_participation_condition is None and previous_contest_id is not None) or \
+            (previous_participation_condition is not None and previous_contest_id is None):
+        raise InsufficientData("previous contest", "id or condition")
