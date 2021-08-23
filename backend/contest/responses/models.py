562aa32648fa5ea225eef90e1dfea9055938c8d4
Marat Khisamutdinov <maratbest6@gmail.com>
2021-08-23 12:14:57 +0300

    add: new classes to model, new fields to classes

--- backend/contest/responses/models.py
+++ backend/contest/responses/models.py
@@ -9,30 +9,6 @@ from sqlalchemy.ext.hybrid import hybrid_property
 db = get_current_db()
 
 
-class Response(db.Model):
-    """
-    Class describing a Response model.
-
-    work_id: id of the user's work
-    user_id: id of the user
-    contest_id: id of the contest
-
-    statuses: responses statuses
-    answers: answers in user response
-    """
-
-    work_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey(UserInContest.user_id))
-    contest_id = db.Column(db.Integer, db.ForeignKey(UserInContest.contest_id))
-    statuses = db.relationship('ResponseStatus', backref='response', lazy=True, cascade="all, delete")
-    answers = db.relationship('ResponseAnswer', backref='response', lazy='dynamic', cascade="all, delete")
-
-    @hybrid_property
-    def mark(self):
-        if len(self.statuses) > 0:
-            return self.statuses[-1].mark
-
-
 class ResponseStatusEnum(enum.Enum):
     """
     Class enumerating statuses of user's work.
@@ -41,38 +17,59 @@ class ResponseStatusEnum(enum.Enum):
     accepted: accepted work
     rejected: rejected work
     appeal: work sent for appeal
-    revision: work sent for revision
+    correction: work sent for correction
     """
 
+    in_progress = 'InProgress'
     not_checked = 'NotChecked'
     accepted = 'Accepted'
     rejected = 'Rejected'
     appeal = 'Appeal'
-    revision = 'Revision'
+    correction = 'Correction'
 
 
 work_status = {status.value: status for status in ResponseStatusEnum}
 
 
-class ResponseStatus(db.Model):
-    """
-    Class describing a Response Status model.
+def add_user_response(db_session, user_id, contest_id):
+    user_work = Response(
+        user_id=user_id,
+        contest_id=contest_id,
+        status=work_status['InProgress']
+    )
+    db_session.add(user_work)
+    return user_work
 
-    status_id: id of the status
+
+class Response(db.Model):
+    """
+    Class describing a Response model.
+
     work_id: id of the user's work
-    timestamp: timestamp to determine the timeline
-    status: status of the work
-    mark: mark of the work
+    user_id: id of the user
+    contest_id: id of the contest
+    start_time: start time of the olympiad
+    finish_time: finish time of the olympiad
+    status: status of the response
 
-    appeal: an appeal which is linked to this status
+    appeal: appeal to user response
+    answers: answers in user response
     """
 
-    status_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    work_id = db.Column(db.Integer, db.ForeignKey(Response.work_id))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    work_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(UserInContest.user_id))
+    contest_id = db.Column(db.Integer, db.ForeignKey(UserInContest.contest_id))
+    start_time = db.Column(db.DateTime, default=datetime.utcnow())
+    finish_time = db.Column(db.DateTime, default=datetime.utcnow())
     status = db.Column(db.Enum(ResponseStatusEnum), nullable=False)
-    mark = db.Column(db.Float)
-    appeal = db.relationship('Appeal', backref='status', lazy=True, uselist=False)
+
+    appeal = db.relationship('Appeal', backref='response', lazy='dynamic', uselist=False, cascade="all, delete")
+    answers = db.relationship('ResponseAnswer', backref='response', lazy='dynamic', cascade="all, delete")
+
+    @hybrid_property
+    def mark(self):
+        if len(self.statuses) > 0:
+            return self.statuses[-1].mark
 
 
 class AppealStatusEnum(enum.Enum):
@@ -97,17 +94,13 @@ class Appeal(db.Model):
     Class describing a Appeal for the user's work.
 
     appeal_id: id of the user's appeal
-    work_status: the status of the work on which the appeal was submitted
+    work_id: the id of the work appealed against
     appeal_status: status of the appeal
-    appeal_message: student's message for appeal
-    appeal_response: expert's response for user's appeal
     """
 
     appeal_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    work_status = db.Column(db.Integer, db.ForeignKey(ResponseStatus.status_id))
-    appeal_status = db.Column(db.Enum(AppealStatusEnum), nullable=False)
-    appeal_message = db.Column(db.Text)
-    appeal_response = db.Column(db.Text)
+    work_id = db.Column(db.Integer, db.ForeignKey(Response.work_id))
+    appeal_status = db.Column(db.Enum(AppealStatusEnum), nullable=False)  # TODO Сообщения создавать на стороне другой
 
 
 class ResponseFiletypeEnum(enum.Enum):
@@ -137,73 +130,113 @@ class ResponseFiletypeEnum(enum.Enum):
 filetype_dict = {filetype.value: filetype for filetype in ResponseFiletypeEnum}
 
 
-class ResponseAnswer(db.Model):
+class AnswerEnum(enum.Enum):
+    PlainAnswer = "PlainAnswer"
+    RangeAnswer = "RangeAnswer"
+    MultipleChoiceAnswer = "MultipleChoiceAnswer"
+    BaseAnswer = "BaseAnswer"
+
+
+class BaseAnswer(db.Model):
     """
-    Class describing user's answers in response.
+    Base class for user's answer
 
     answer_id: id of the user's answer
     work_id: id of the user's work
     task_id: id of the task
-    answer: user's answer as a file
-    filetype: user's answer filetype
     """
 
     answer_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     work_id = db.Column(db.Integer, db.ForeignKey('response.work_id'))
     task_id = db.Column(db.Integer, db.ForeignKey(f'{Task.__tablename__}.task_id'))
-    answer = db.Column(db.LargeBinary, nullable=False)
-    filetype = db.Column(db.Enum(ResponseFiletypeEnum), nullable=False)
+    answer_type = db.Column(db.Enum(AnswerEnum), nullable=False)
 
-    def update(self, answer_new=None, filetype_new=None):
+    __mapper_args__ = {
+        'polymorphic_identity': AnswerEnum.PlainAnswer,
+        'polymorphic_on': answer_type
+    }
+
+
+class RangeAnswer(BaseAnswer):
+    """
+    Class describing answer for range task
+
+    answer_id: id of the answer
+    answer: user answer
+    """
+
+    answer_id = db.Column(db.Integer, db.ForeignKey(BaseAnswer.answer_id), primary_key=True)
+    answer = db.Column(db.Float, nullable=False)
+
+    __mapper_args__ = {
+        'polymorphic_identity': AnswerEnum.RangeAnswer,
+    }
+
+
+class PlainAnswer(BaseAnswer):
+    """
+    Class describing answer for plain task
+
+    answer_id: id of the answer
+    answer: user's answer as a file
+    filetype: user's answer filetype
+    """
+
+    answer_id = db.Column(db.Integer, db.ForeignKey(BaseAnswer.answer_id), primary_key=True)
+    answer_text = db.Column(db.Text, nullable=True)
+    answer_file = db.Column(db.LargeBinary, nullable=True)
+    filetype = db.Column(db.Enum(ResponseFiletypeEnum), nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': AnswerEnum.PlainAnswer,
+    }
+
+    def update(self, answer_new=None, filetype_new=None):  # TODO FIX
         if answer_new is not None:
-            self.answer = answer_new
+            self.answer_file = answer_new
         if filetype_new is not None:
             self.filetype = filetype_dict[filetype_new]
 
 
-def add_user_response(db_session, user_id, contest_id):
-    user_work = Response(
-        user_id=user_id,
-        contest_id=contest_id
-    )
-    db_session.add(user_work)
-    return user_work
+class MultipleChoiceAnswer(BaseAnswer):
+    """
+    Class describing answer for multiple choice answer
+
+    answer_id: id of the answer
+    """
+
+    answer_id = db.Column(db.Integer, db.ForeignKey(BaseAnswer.answer_id), primary_key=True)
+
+    answers = db.relationship('MultipleUserAnswer', lazy='dynamic', cascade="all, delete")
+
+    __mapper_args__ = {
+        'polymorphic_identity': AnswerEnum.MultipleChoiceAnswer,
+    }
 
 
-def add_response_status(work_id, status=None, mark=None):
-    if status is None:
-        new_status = work_status['NotChecked']
-    else:
-        new_status = work_status.get(status)
-    if mark is None:
-        response_status = ResponseStatus(
-            work_id=work_id,
-            status=new_status
-        )
-    else:
-        response_status = ResponseStatus(
-            work_id=work_id,
-            status=new_status,
-            mark=mark
-        )
-    return response_status
+class MultipleUserAnswer(db.Model):
+    """
+    Class describing each user answer for multiple choice task
+    """
+
+    answer_id = db.Column(db.Integer, db.ForeignKey(MultipleChoiceAnswer.answer_id), primary_key=True)
+    text = db.Column(db.Text, nullable=False)
 
 
-def add_response_answer(work_id, task_id, answer, filetype):
-    response_answer = ResponseAnswer(
-        work_id=work_id,
-        task_id=task_id,
-        answer=answer,
-        filetype=filetype_dict[filetype]
-    )
-    return response_answer
+# def add_response_answer(work_id, task_id, answer, filetype):        #TODO
+#    response_answer = ResponseAnswer(
+#        work_id=work_id,
+#        task_id=task_id,
+#        answer=answer,
+#        filetype=filetype_dict[filetype]
+#    )
+#    return response_answer
 
 
 def add_response_appeal(status_id, message):
     appeal = Appeal(
         work_status=status_id,
         appeal_status=appeal_status['UnderReview'],
-        appeal_message=message
     )
     return appeal
 
