90f8b689a512e6b9cb31d91f768f8f21065dd460
Mikhail <16112000m@gmail.com>
2021-09-16 18:32:37 +0300

    add: enrolled flag to contests response for participant

--- backend/contest/tasks/participant/schemas.py
+++ backend/contest/tasks/participant/schemas.py
@@ -1,5 +1,12 @@
+from datetime import timedelta
+
 from marshmallow import Schema, fields
+from marshmallow_enum import EnumField
+
 from common import fields as common_fields
+from contest.tasks.model_schemas.location import OlympiadLocationSchema
+from contest.tasks.model_schemas.olympiad import TargetClassSchema, BaseContestSchema, SimpleContestSchema
+from contest.tasks.models import ContestHoldingTypeEnum, UserStatusEnum
 
 
 class AnswersInTaskRequestTaskParticipantSchema(Schema):
@@ -17,3 +24,16 @@ class AllTaskResponseTaskParticipantSchema(Schema):
 
 class EnrollRequestTaskParticipantSchema(Schema):
     location_id = fields.Int(required=False)
+
+
+class SimpleContestWithFlagResponseTaskParticipantSchema(Schema):
+    contest = fields.Nested(SimpleContestSchema, required=False, dump_only=True)
+    enrolled = fields.Boolean(required=True)
+
+
+# For filter query
+
+
+class FilterSimpleContestResponseTaskParticipantSchema(Schema):
+    contest_list = fields.Nested(nested=SimpleContestWithFlagResponseTaskParticipantSchema, many=True)
+    count = fields.Integer()
