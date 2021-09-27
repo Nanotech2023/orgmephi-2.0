ec5cef6fcc336d068d46cc541c0270eea547925a
Artem Chernyshov <a-chernyshov@protonmail.com>
2021-09-27 19:44:15 +0300

    add: home component

--- frontend/src/app/contests/contests.routing.module.ts
+++ frontend/src/app/contests/contests.routing.module.ts
@@ -4,10 +4,26 @@ import { AuthGuardService } from '@/shared/auth.guard.service'
 import { ContestListComponent } from '@/contests/containers/contest-list/contest-list.component'
 import { ContestDetailsComponent } from '@/contests/containers/contest-details/contest-details.component'
 import { ContestRegistrationComponent } from '@/contests/containers/contest-registration/contest-registration.component'
+import { HomeComponent } from '@/contests/containers/home/home.component'
+import { ContestListItemComponent } from '@/contests/components/contest-list-item/contest-list-item.component'
+import { ContestAssignmentComponent } from '@/contests/containers/contest-assignment/contest-assignment.component'
+
+
+export const COMPONENTS = [
+    ContestListComponent,
+    ContestDetailsComponent,
+    ContestListItemComponent,
+    ContestRegistrationComponent,
+    ContestAssignmentComponent,
+    HomeComponent
+]
 
 
 const routes: Routes =
     [
+        {
+            path: '/home', component: HomeComponent, canActivate: [ AuthGuardService ]
+        },
         {
             path: 'contests', component: ContestListComponent, canActivate: [ AuthGuardService ]
         },
